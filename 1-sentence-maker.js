3eb299f2406dec678d900608acf6336475a48915
kirungo <terrikirungo@gmail.com>
2026-04-12 09:33:10 +0300

    Create 1-sentence-maker.js

---
+++ 1-sentence-maker.js
@@ -0,0 +1,22 @@
+let adjective = "beautiful";
+let noun = "woman";
+let verb = "dying";
+let place = "yacht";
+let adjective2 = "big";
+let noun2 = "mangoes";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// second story
+adjective = "tall";
+noun = "man";
+verb = "starting";
+place = "forest";
+adjective2 = "small";
+noun2 = "apples";
+
+let secondStory = "In a land far away, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
