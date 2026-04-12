2aa540affda3d8a3b8b5f71bd91362649e9250bf
kirungo <terrikirungo@gmail.com>
2026-04-12 09:13:54 +0300

    build a trivia bot using vanilla javascript

---
+++ trivia-bot.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "codeCampBot";
+let botLocation = "Edinburg";
+let favoriteLanguage = "JavaScript";
+
+// This: 
+// console.log("My name is " + botName + " and I live on " + botLocation + "."); 
+// can also be written as:
+console.log(`My name is ${botName} and I live in ${botLocation}.`);
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `${favoriteLanguage} is fun.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is also easy to learn.`;
+console.log(codingFact);
+
+codingFact = `Would you like to learn ${favoriteLanguage} today?`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
